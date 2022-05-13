8f2a51aee8741056f6550f7bb6cb2c65f0d2df72
AidarS <nurotanec@bk.ru>
2022-05-13 23:50:54 +0600

    added fetch

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ import {timer} from './modules/timer.js';
 import './modules/acc.js';
 import './modules/burger.js';
 import './modules/fly.js';
+import './modules/fetchData.js';
 
 {
   const init = () => {
